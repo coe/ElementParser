444b159b1596cdcfb6b5798cd5efd5e5b3ae719e
coe <coe.app@gmail.com>
2022-07-18 21:30:06 +0900

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ElementParser
+
+A description of this package.
---
+++ Tests/ElementParserTests/ElementParserTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import ElementParser
+
+final class ElementParserTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(ElementParser().text, "Hello, World!")
+    }
+}
